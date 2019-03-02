dac17efdc8c5e612713c312ea8fd93de13a6f69b
mkudyukov <mkudyukov@a-k-d.ru>
2019-03-02 14:22:21 +0300

    ++

--- src/cmd/csp-utils-rs/csp-utils-rs.go
+++ src/cmd/csp-utils-rs/csp-utils-rs.go
@@ -8,6 +8,11 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"os/exec"
+	"strings"
+	exec2 "github.com/adlane/exec"
+
+	"time"
+	"strconv"
 )
 
 var conf struct {
@@ -21,15 +26,135 @@ var (
 	inst         = flag.Bool("inst", false, "Install certificate")
 	uninst       = flag.Bool("uninst", false, "Uninstall certificate")
 	store        = flag.String("store", "root", "Storage")
-	config       = ""
-	command		 = "crtmgr"
+	t            = flag.String("thumbprint", "", "thumbprint for deletion")
+	config       = flag.String("config", "", "config")
+	command		 = "certmgr"
+	Ctx          exec2.ProcessContext
+	done         chan struct{}
+	errorc       chan struct{}
+	attempt      = 1
+	maxattempt   = 5
 )
+
+type reader struct {
+}
+
+func (*reader) OnData(b []byte) bool {
+
+	fmt.Println("===")
+
+	fmt.Print(string(b))
+
+	if strings.Contains(string(b), "(o)OK, (c)Cancel") {
+
+		fmt.Println("-= Sending o =-")
+
+		Ctx.Send("o\n")
+
+	}
+
+	if strings.Contains(string(b), "Please choose index") {
+
+		fmt.Println("-= Sending 2 =-")
+
+		Ctx.Send("2\n")
+
+	}
+
+	if strings.Contains(string(b), "[ErrorCode: 0x00000000]") {
+		log.Println("Done")
+		done <- struct{}{}
+	}
+
+	return false
+}
+
+func (*reader) OnError(b []byte) bool {
+	fmt.Print(string(b))
+	if !strings.Contains(string(b), "WARNING") {
+		errorc <- struct{}{}
+	}
+	return false
+}
+
+
+func (*reader) OnTimeout() {
+	log.Println("Died on timeout")
+	errorc <- struct{}{}
+}
+
+func checkRoot () {
+	cmd := exec.Command("id", "-u")
+	output, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// output has trailing \n
+	// need to remove the \n
+	// otherwise it will cause error for strconv.Atoi
+	// log.Println(output[:len(output)-1])
+
+	// 0 = root, 501 = non-root user
+	i, err := strconv.Atoi(string(output[:len(output)-1]))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if i != 0 {
+		log.Fatal("This program must be run as root! (sudo)")
+	}
+
+}
+
+func execute(command string, args ...string) {
+
+ATTEMPTLOOP:
+	for {
+
+		log.Println("\n-----------------",attempt,"/",maxattempt,"-----------------")
+		log.Println("Command:", command, args)
+
+		Ctx = exec2.InteractiveExec(command, args...)
+
+		r := reader{}
+		go Ctx.Receive(&r, 10*time.Second)
+
+		for {
+			select {
+			case <-done:
+				os.Exit(0)
+			case <-errorc:
+				if attempt <= maxattempt {
+					log.Println("Try again")
+					attempt++
+					Ctx.Cancel()
+					Ctx.Stop()
+					time.Sleep(1*time.Second)
+					continue ATTEMPTLOOP
+				}
+				log.Println("Finished with error")
+				os.Exit(1)
+			}
+		}
+	}
+
+	return
+
+}
 
 func main() {
 
-	config = os.Getenv("CONFIG")
+	//checkRoot()
 
-	data, err := ioutil.ReadFile(fmt.Sprintf("%s", config))
+	done = make(chan struct{})
+	errorc = make(chan struct{})
+
+	flag.Parse()
+
+	data, err := ioutil.ReadFile(fmt.Sprintf("%s", *config))
 
 	if err != nil {
 
@@ -37,65 +162,23 @@ func main() {
 	}
 
 	if err := json.Unmarshal(data, &conf); err != nil {
-		log.Fatal(err.Error())
+		log.Fatal("Config parse error: ", err.Error())
 	}
 
-	flag.Parse()
-
-	if _, err := os.Stat(*f); err != nil {
+	if _, err := os.Stat(*f); err != nil && *inst {
 		log.Fatal("Certificate file not found", *f)
 	}
 
+	if *uninst && *t == "" {
+		log.Fatal("Must specify Thumbprint")
+	}
+
 	if *inst {
-
-		cmd := exec.Command(command, "-inst", "-store", *store, "-f", *f)
-		//cmd := exec.Command("echo", "123")
-		//cmd := exec.Command("openssl", "x509", "-in", "../cert/cert.pem", "-text")
-
-		//stdin, err := cmd.StdinPipe()
-		//if err != nil {
-		//	log.Fatal(err.Error())
-		//}
-
-		stdout, err := cmd.StdoutPipe()
-		if err != nil {
-			log.Fatal("Stdout pipe init error ", err.Error())
-		}
-
-
-		defer func (){
-			stdout.Close()
-		}()
-
-		err = cmd.Start()
-		if err != nil {
-
-			resp, err2 := ioutil.ReadAll(stdout)
-			if err2 != nil {
-				log.Println("Stdout read error ", err2.Error())
-			}
-
-			log.Println(string(resp))
-
-			log.Fatal("Run error ", err.Error())
-		}
-
-		resp, err := ioutil.ReadAll(stdout)
-		if err != nil {
-			log.Fatal("Read error ", err.Error())
-		}
-
-		log.Println(string(resp))
-
-		if err := cmd.Wait(); err != nil {
-			log.Fatal("Wait error ", err.Error())
-		}
-
-		return
+		execute(conf.Csp.CprospPath+command, "-inst", "-store", *store, "-f", *f)
 	}
 
 	if *uninst {
-		return
+		execute(conf.Csp.CprospPath+command, "-delete", "-store", *store, "-thumbprint", *t)
 	}
 
 	log.Fatal("Unknown command, must specify -inst or -uninst, view --help for details")
@@ -107,3 +190,4 @@ func main() {
 	//}()
 
 }
+
